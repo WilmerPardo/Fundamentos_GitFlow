e2e48980195e3becefed7ca5f43152651fc4c834
WilmerPardo <wilmer.pardo@unl.edu.ec>
2023-11-10 18:05:28 -0500

    Desarrollo: Creando logica. WP

---
+++ main.java
@@ -0,0 +1,10 @@
+
+
+ public class main {
+    public static void main(String[] args) {
+        app calculadora = new app();
+        calculadora.calcular();
+        }
+}
+    
+    
---
+++ src/app.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class app {
+    private int num1;
+    private int num2;
+    private char operator;
+
+    public app() {
+    }
+
+    public double calcular(){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Ingrese el primer número: ");
+        num1 = input.nextInt();
+
+        System.out.print("Ingrese el segundo número: ");
+        num2 = input.nextInt();
+
+        System.out.print("Ingrese la operación (+, -, *, /): ");
+        operator = input.next().charAt(0);
+
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if(num2 == 0){
+                    System.out.println("No se puede dividir entre 0");
+                    return 0;
+                }
+                result = (double)(num1) / (double)(num2);
+                break;
+            default:
+                System.out.println("Operación inválida");
+                return 0;
+        }
+
+        System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
+        return result;
+    }
+
+
+}
